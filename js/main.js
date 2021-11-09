f1c5b9cdfce8b32ea9f8bd10bb5d65ea02853caf
Martín De Angelis <madeangelis@gmail.com>
2021-11-09 11:28:09 -0300

    update year

--- js/main.js
+++ js/main.js
@@ -1,91 +1,93 @@
 jQuery(document).ready(function ($) {
-
-  'use strict';
+  "use strict";
 
   $(function () {
-
     // Vars
-    var modBtn = $('#modBtn'),
-      modal = $('#modal'),
-      close = modal.find('.close-btn img'),
-      modContent = modal.find('.modal-content');
+    var modBtn = $("#modBtn"),
+      modal = $("#modal"),
+      close = modal.find(".close-btn img"),
+      modContent = modal.find(".modal-content");
 
-    // open modal when click on open modal button 
-    modBtn.on('click', function () {
-      modal.css('display', 'block');
-      modContent.removeClass('modal-animated-out').addClass('modal-animated-in');
+    // open modal when click on open modal button
+    modBtn.on("click", function () {
+      modal.css("display", "block");
+      modContent
+        .removeClass("modal-animated-out")
+        .addClass("modal-animated-in");
     });
 
-    // close modal when click on close button or somewhere out the modal content 
-    $(document).on('click', function (e) {
+    // close modal when click on close button or somewhere out the modal content
+    $(document).on("click", function (e) {
       var target = $(e.target);
       if (target.is(modal) || target.is(close)) {
-        modContent.removeClass('modal-animated-in').addClass('modal-animated-out').delay(300).queue(function (next) {
-          modal.css('display', 'none');
-          next();
-        });
+        modContent
+          .removeClass("modal-animated-in")
+          .addClass("modal-animated-out")
+          .delay(300)
+          .queue(function (next) {
+            modal.css("display", "none");
+            next();
+          });
       }
     });
-
   });
 
   // on click event on all anchors with a class of scrollTo
-  $('a.scrollTo').on('click', function () {
-
+  $("a.scrollTo").on("click", function () {
     // data-scrollTo = section scrolling to name
-    var scrollTo = $(this).attr('data-scrollTo');
-
+    var scrollTo = $(this).attr("data-scrollTo");
 
     // toggle active class on and off. added 1/24/17
     $("a.scrollTo").each(function () {
-      if (scrollTo == $(this).attr('data-scrollTo')) {
-        $(this).addClass('active');
+      if (scrollTo == $(this).attr("data-scrollTo")) {
+        $(this).addClass("active");
       } else {
-        $(this).removeClass('active');
+        $(this).removeClass("active");
       }
     });
 
-
-    // animate and scroll to the sectin 
-    $('body, html').animate({
-
-      // the magic - scroll to section
-      "scrollTop": $('#' + scrollTo).offset().top
-    }, 1000);
+    // animate and scroll to the sectin
+    $("body, html").animate(
+      {
+        // the magic - scroll to section
+        scrollTop: $("#" + scrollTo).offset().top,
+      },
+      1000
+    );
     return false;
-
-  })
-
+  });
 
   $(".menu-icon").click(function () {
     $(this).toggleClass("active");
     $(".overlay-menu").toggleClass("open");
   });
-
 });
-
 
 //-----------Play Videos-------------
 
 var baseVideo = "https://www.youtube.com/embed/";
 
-$('.portfolio-item').click(function () {
-  console.log('click');
-  var video = $(this).data('video');
-  $('#video iframe').attr('src', baseVideo + video);
-  $('#video').removeClass('invisible');
-  $('#about').addClass('invisible');
+$(".portfolio-item").click(function () {
+  console.log("click");
+  var video = $(this).data("video");
+  $("#video iframe").attr("src", baseVideo + video);
+  $("#video").removeClass("invisible");
+  $("#about").addClass("invisible");
 });
 
-
-$('#video').click(function () {
-  $('#video').addClass('invisible');
-  $('#video iframe').attr('src', "");
-  $('#about').addClass('invisible');
+$("#video").click(function () {
+  $("#video").addClass("invisible");
+  $("#video iframe").attr("src", "");
+  $("#about").addClass("invisible");
 });
 
-$('#videoImage').click(function () {
-  $('#video').addClass('invisible');
-  $('#video iframe').attr('src', "");
-  $('#about').addClass('invisible');
+$("#videoImage").click(function () {
+  $("#video").addClass("invisible");
+  $("#video iframe").attr("src", "");
+  $("#about").addClass("invisible");
 });
+
+// Print year
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("currentYear").innerHTML = year;
